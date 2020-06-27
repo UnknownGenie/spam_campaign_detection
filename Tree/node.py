f537a28240bcb89dfc135fe41fbac9aeab48e34d
Uknown Genie <00faheem92@gmail.com>
2020-06-27 21:02:39 +0500

    Adding initial files

---
+++ Tree/node.py
@@ -0,0 +1,187 @@
+from collections import defaultdict, namedtuple
+
+class FPNode(object):
+    """A node in an FP tree."""
+
+    def __init__(self, tree, item, count=1):
+        self._tree = tree
+        self._item = item
+        self._count = count
+        self._parent = None
+        self._children = {}
+        self._neighbor = None
+        self._num_leaves=None
+
+
+    @property
+    def header_table(self):
+        return self.tree.header_table
+
+    def add(self, child):
+        """Add the given FPNode `child` as a child of this node."""
+        if not isinstance(child, FPNode):
+            raise TypeError("Can only add other FPNodes as children")
+
+        if child.item not in self._children:
+            self._children[child.item] = child
+            child.parent = self
+
+    def remove(self, child):
+        """Remove the given FPNode `child` as a child of this node."""
+
+        if not isinstance(child, FPNode):
+            raise TypeError("Can only remove other FPNodes as children")
+        if child.item in self._children:
+            del self._children[child.item]
+            
+    def disp(self, ind=1):
+        print('  '*ind, self.name, ' ', self.count)
+        for child in self.children.values():
+            child.disp(ind+1)
+
+    def search(self, item):
+        """
+        Check whether this node contains a child node for the given item.
+        If so, that node is returned; otherwise, `None` is returned.
+        """
+        try:
+            return self._children[item]
+        except KeyError:
+            return None
+
+    def __contains__(self, item):
+        return item in self._children
+
+    @property
+    def tree(self):
+        """The tree in which this node appears."""
+        return self._tree
+
+    @property
+    def item(self):
+        """The item contained in this node."""
+        return self._item
+
+    @property
+    def count(self):
+        """The count associated with this node's item."""
+        return self._count
+
+    def increment(self):
+        """Increment the count associated with this node's item."""
+        if self._count is None:
+            raise ValueError("Root nodes have no associated count.")
+        self._count += 1
+
+    @property
+    def root(self):
+        """True if this node is the root of a tree; false if otherwise."""
+        return self._item is None and self._count is None
+
+    @property
+    def leaf(self):
+        """True if this node is a leaf in the tree; false if otherwise."""
+        return len(self._children) ==0
+
+    @property
+    def parent(self):
+        """The node's parent"""
+        return self._parent
+
+    @parent.setter
+    def parent(self, value):
+        if value is not None and not isinstance(value, FPNode):
+            raise TypeError("A node must have an FPNode as a parent.")
+        if value and value.tree is not self.tree:
+            raise ValueError("Cannot have a parent from another tree.")
+        self._parent = value
+
+    @property
+    def neighbor(self):
+        """
+        The node's neighbor; the one with the same value that is "to the right"
+        of it in the tree.
+        """
+        return self._neighbor
+
+    @neighbor.setter
+    def neighbor(self, value):
+        if value is not None and not isinstance(value, FPNode):
+            raise TypeError("A node must have an FPNode as a neighbor.")
+        if value and value.tree is not self.tree:
+            raise ValueError("Cannot have a neighbor from another tree.")
+        self._neighbor = value
+
+    @property
+    def children(self):
+        """The nodes that are children of this node."""
+        return tuple(self._children.values())
+    #         print (tuple(self._children.values()))
+
+    @property
+    def num_leaves(self):
+        if self.leaf: return 1
+        elif self._num_leaves is None: 
+            self._num_leaves=sum([i.num_leaves for i in self.children])
+        return self._num_leaves 
+        
+    
+
+    def cond1(self,min_num_children):
+        return len(self._children) > min_num_children
+    
+    
+    def cond2(self,freq_threshold):
+        count=0
+        n=0
+        for child in self.children:
+            count+=child.count
+            n+=1
+        return count/n >= freq_threshold
+
+    def cond3(self,n_obf_features):
+        temp_node=self
+        while(not temp_node.root):
+            if temp_node.item not in n_obf_features: return True
+            temp_node=temp_node.parent
+
+        return False
+    def cond4(self,min_num_messages):
+        return self.num_leaves>=min_num_messages
+
+    def cond(self,param):
+        return self.cond1(param[0]) and self.cond2(param[1]) and self.cond3(param[2]) and self.cond4(param[3])
+
+    def get_items(self,items):
+        items.append(self.item)
+        
+        if self.leaf: return
+            
+        for child in self.children:
+            child.get_items(items)
+
+        
+         
+    def dfs(self,param,campaign):
+        if self.leaf : 
+            return
+        for child in self.children:
+            child.dfs(param,campaign)
+        
+        if self.cond(param):
+            self.parent.remove(self)
+            campaign.append(self)
+            print(campaign[-1].item)
+            return        
+        
+
+    def inspect(self, depth=0):
+        print(("  " * depth) + repr(self))
+        for child in self.children:
+            child.inspect(depth + 1)
+
+    def __repr__(self):
+        if self.root:
+            return "<%s (root)>" % type(self).__name__
+        return "<%s %r (%r)>" % (type(self).__name__, self.item, self.count)
+
---
+++ Tree/tree.py
@@ -0,0 +1,149 @@
+from collections import defaultdict, namedtuple
+from Tree.node import FPNode
+
+def defdict(dicts):
+    return defaultdict(lambda: 0, dicts)
+
+class FPTree(object):
+    """
+    An FP tree.
+    This object may only store transaction items that are hashable
+    (i.e., all items must be valid as dictionary keys or set members).
+    """
+
+    Route = namedtuple("Route", "head tail")
+
+    def __init__(self, items = defaultdict(lambda: 0)):
+        # The root node of the tree.
+        self._root = FPNode(self, None, None)
+
+        # A dictionary mapping items to the head and tail of a path of
+        # "neighbors" that will hit every node containing that item.
+        self._routes = {}
+        self._header_table=items
+
+        
+
+    @property
+    def header_table(self):
+        """The header table of the tree"""
+        return self._header_table
+
+    #more like updates the stuff
+    def header_table_sort(self):
+        
+        self._header_table=defdict({k: v for k, v in sorted(self._header_table.items(), key=lambda item: item[1],reverse=True)})
+
+    @property
+    def root(self):
+        """The root node of the tree."""
+        return self._root
+    
+    def add_on_fly(self, transaction):
+        pass
+    
+    def add_traditional(self, transaction):
+        """Add a transaction to the tree."""
+        id,content=transaction[-1],transaction[0]
+        transaction=[content]+[k for k,v in self.header_table.items() if k in transaction[1:-1]]+[id]
+        point = self._root
+        for item in transaction:
+            next_point = point.search(item)
+            if next_point:
+                # There is already a node in this tree for the current
+                # transaction item; reuse it.
+                next_point.increment()
+            else:
+                # Create a new point and add it as a child of the point we're
+                # currently looking at.
+                next_point = FPNode(self, item)
+                point.add(next_point)
+
+                # Update the route of nodes that contain this item to include
+                # our new node.
+                self._update_route(next_point)
+            point = next_point
+        
+    def add(self,transaction, on_fly = True):
+        if all(isinstance(i, list) for i in transaction):
+            for transaction in transaction:
+                if on_fly:
+                    self.add_on_fly(transaction)
+                else:
+                    self.add_traditional(transaction)
+        else:
+            print("provide transaction as list")
+
+    
+
+    def _update_route(self, point):
+        """Add the given node to the route through all nodes for its item."""
+        assert self is point.tree
+
+        try:
+            route = self._routes[point.item]
+            route[1].neighbor = point  # route[1] is the tail
+            self._routes[point.item] = self.Route(route[0], point)
+        except KeyError:
+            # First node for this item; start a new route.
+            self._routes[point.item] = self.Route(point, point)
+
+    def items(self):
+        """
+        Generate one 2-tuples for each item represented in the tree. The first
+        element of the tuple is the item itself, and the second element is a
+        generator that will yield the nodes in the tree that belong to the item.
+        """
+        for item in self._routes.keys():
+            yield (item, self.nodes(item))
+
+    def nodes(self, item):
+        """
+        Generate the sequence of nodes that contain the given item.
+        """
+
+        try:
+            node = self._routes[item][0]
+        except KeyError:
+            return
+
+        while node:
+            yield node
+    #             print(node)
+            node = node.neighbor
+    #         print(node)
+
+    def prefix_paths(self, item):
+        """Generate the prefix paths that end with the given item."""
+
+        def collect_path(node):
+            path = []
+            while node and not node.root:
+                path.append(node)
+                node = node.parent
+            path.reverse()
+            return path
+
+        return (collect_path(node) for node in self.nodes(item))
+
+    def inspect(self):
+        print("Tree:")
+        self.root.inspect(1)
+
+        print()
+        print("Routes:")
+        for item, nodes in self.items():
+            print("  %r" % item)
+            for node in nodes:
+                print("    %r" % node)
+
+    def getcampaign(self,param):
+#         print(self.header_table)
+        campaign=[]
+        self.root.dfs(param,campaign)
+        itemlist=[]
+        for item in campaign:
+            t=[]
+            item.get_items(t)
+            itemlist+=[t]
+        return itemlist
